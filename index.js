144d55fa4f3aa44e9dcb42609246875d410d4994
TiroV <91702935+TiroV@users.noreply.github.com>
2022-01-27 14:15:47 -0800

    Allows localhost:1234 access to project
    When committed, allows access to the other side of the project

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //For using the cors package
 const cors = require('cors');
-let allowedOrigins = ['http:://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http:://localhost:8080', 'http://localhost:1234', 'http://testsite.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
